665ac2c68740ff0d40143cd3a38e8c7c4e08736f
Garrett Burroughs <gab8un@virginia.edu>
2023-01-31 17:14:41 +0000

    Up to problem 9 on lab 1

--- 01-SQL/Labs/Lab_01_SQL_Query_Fundamentals.sql
+++ 01-SQL/Labs/Lab_01_SQL_Query_Fundamentals.sql
@@ -4,7 +4,6 @@ SELECT *
 FROM northwind.products;
 
 -- 1). Product Name and Unit/Quantity
-FROM northwind.products
 SELECT id AS product_id,
        product_name
 FROM northwind.products;
@@ -45,11 +44,16 @@ WHERE list_price < 20
 ORDER BY list_price DESC;
 
 -- 6). Product ID, Name & List Price Costing Between $15 and $20
+SELECT id AS product_id, product_name, list_price FROM northwind.products WHERE list_price >= 15 and list_price <= 20;
 -- 7). Product Name & List Price Costing Above Average List Price
+SELECT product_name, list_price FROM northwind.products WHERE list_price > (SELECT AVG(list_price) FROM northwind.products);
 -- 8). Product Name & List Price of 10 Most Expensive Products
+SELECT product_name, list_price FROM northwind.products ORDER BY list_price DESC LIMIT 10;
 -- 9). Count of Current and Discontinued Products
+SELECT CASE discontinued WHEN 1 THEN 'yes' ELSE 'no' END AS is_discontinued FROM northwind.products ORDER BY discontinued;
 -- 10). Product Name, Units on Order and Units in Stock
 --      Where Quantity In-Stock is Less Than the Quantity On-Order.
+
  -- EXTRA CREDIT -----------------------------------------------------
  -- 11). Products with Supplier Company & Address Info
 -- 12). Number of Products per Category With Less Than 5 Units
